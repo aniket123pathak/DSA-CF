b8a81fe21c06e44d1209db104e2c85d322a04ff7
Aniket Pathak <aniketpathak581@gmail.com>
2025-10-28 21:36:18 +0530

    temp

---
+++ STACK/twoStack.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+class twoStack{
+    private : 
+        int top1;
+        int top2;
+        int size;
+
+    public:
+        twoStack(int size){
+            top1=-1;
+            top2=size;
+            this->size=size;
+        } 
+
+        void push1(int element){
+            
+        }
+        void push2(int element){
+
+        }
+        int pop1(){
+
+        }
+        int pop2(){
+
+        }
+        int isEmpty(){
+
+        }
+        int peek(){
+
+        }
+};
+
