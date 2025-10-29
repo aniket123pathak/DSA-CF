6e6721b15b318fd63fc17303ae47f673467098b3
Aniket Pathak <aniketpathak581@gmail.com>
2025-10-29 06:20:06 +0530

    two stack

--- STACK/twoStack.cpp
+++ STACK/twoStack.cpp
@@ -6,31 +6,44 @@ class twoStack{
         int top1;
         int top2;
         int size;
+        int* arr;
 
     public:
         twoStack(int size){
             top1=-1;
             top2=size;
             this->size=size;
+            arr = new int[size];
         } 
 
         void push1(int element){
-            
+            if(top2-top1>1){
+                top1++;
+                arr[top1]=element;
+            }
         }
         void push2(int element){
-
+            if(top2-top1>1){
+                top2--;
+                arr[top2]=element;
+            }
         }
         int pop1(){
-
+            if(top1!=-1){
+                int d = arr[top1];
+                top1--;
+                return d;
+            }
         }
         int pop2(){
-
+            if(top2!=size){
+                int d = arr[top2];
+                top2++;
+                return d;
+            }
         }
         int isEmpty(){
-
-        }
-        int peek(){
-
+            return top2==size && top1==-1;
         }
 };
 
