8dd2ca00b2170ee28c6047d5715c402805cd0f26
Aniket Pathak <aniketpathak581@gmail.com>
2025-11-06 14:00:30 +0530

    new que

---
+++ CF103.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+    public:
+        void solve()
+        {
+            long long x,y;
+            cin>>x>>y;
+            if(x==y || y==1){
+                cout<<"-1\n";
+                return;
+            }
+
+            if(x<y){
+                cout<<"2\n";
+                return;
+            }
+
+            if(x-1>y){
+                cout<<"3\n";
+                return;
+            }
+
+            cout<<"-1\n";
+
+        }
+};
+
+int main () {
+    Solution s;
+    int tc;
+    cin>>tc;
+    while(tc-->0)
+    {
+        s.solve();
+    }
+    return 0;
+}
