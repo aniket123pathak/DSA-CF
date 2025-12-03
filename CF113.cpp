6707256647cea26c6b8ba57b00322d945bd7504d
Aniket Pathak <aniketpathak581@gmail.com>
2025-12-03 12:47:30 +0530

    new que

---
+++ CF113.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+    public:
+        void solve()
+        {
+            long long n;
+            cin>>n;
+            if(n%2!=0){
+                cout<<"0\n";
+                return ;
+            }
+            cout<<n/4+1<<endl;
+        }
+};
+
+int main () {
+    Solution s;
+    int tc;
+    cin>>tc;
+    while(tc-->0)
+    {
+        s.solve();
+    }
+    return 0;
+}
