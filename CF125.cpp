de55376c39d37c7ec5620823b31d801ac3904729
Aniket Pathak <aniketpathak581@gmail.com>
2025-12-13 18:56:54 +0530

    new que

--- CF125.cpp
+++ CF125.cpp
@@ -1,31 +1,48 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-class Solution {
-    public:
-        void solve()
+class Solution
+{
+public:
+    void solve()
+    {
+        long long n, i;
+        cin >> n;
+        vector<long long> a(n);
+        for (i = 0; i < n; i++)
         {
-            long long n,i,x,diff=0;
-            cin>>n;
-            vector<long long>hash(1001);
-            for(i=0;i<n;i++){
-                cin>>x;
-                if(hash[x]==0){
-                    hash[x]++;
-                    diff++;
-                }
-            }
-            
+            cin >> a[i];
         }
+        long count = 0;
+
+        long long cur = 0;
+        long long next = 1;
+
+        while (next < n)
+        {
+            if (a[cur] > a[next])
+            {
+                count++;
+                next++;
+            }
+            else
+            {
+                cur = next;
+                next++;
+            }
+        }
+        cout << count << endl;
+    }
 };
 
-int main () {
+int main()
+{
     Solution s;
     int tc;
-    cin>>tc;
-    while(tc-->0)
+    cin >> tc;
+    while (tc-- > 0)
     {
         s.solve();
     }
