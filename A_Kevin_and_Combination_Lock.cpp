3eb4f0162b8d6860cbb15595491729a11bf95338
Aniket Pathak <aniketpathak581@gmail.com>
2025-12-17 19:11:35 +0530

    new que

---
+++ A_Kevin_and_Combination_Lock.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+    public:
+        void solve()
+        {
+            long long n;
+            cin>>n;
+            if(n%33==0){
+                cout<<"YES\n";
+                return;
+            }
+
+            cout<<"NO\n";
+        }
+};
+
+int main () {
+    Solution s;
+    int tc;
+    cin>>tc;
+    while(tc-->0)
+    {
+        s.solve();
+    }
+    return 0;
+}
