46349974611314b7df50398a63f15bfa02fbb6f8
Aniket Pathak <aniketpathak581@gmail.com>
2025-12-24 14:57:38 +0530

    new que

---
+++ C_Blackslex_and_Number_Theory.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define ll long long
+#define v(a,n) vector<long long> a(n)
+#define fin(i,x,n,a) for(i=x;i<n;i++) cin>>a[i]
+
+class Solution {
+public:
+    void solve()
+    {
+        ll n,i;
+        cin>>n;
+        v(a,n);
+        fin(i,0,n,a);
+        sort(a.begin(),a.end());
+        cout<<max(a[0],a[1]-a[0])<<endl;
+    }
+};
+
+int main() {
+    Solution s;
+    int tc;
+    cin >> tc;
+    while (tc-- > 0)
+    {
+        s.solve();
+    }
+    return 0;
+}
