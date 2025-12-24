caa50af9587c0ec3958cbfd6d156d59e87d8aa9f
Aniket Pathak <aniketpathak581@gmail.com>
2025-12-24 14:57:33 +0530

    new que

---
+++ B_Symmetric_Encoding.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define ll long long
+#define v(a,n) vector<long long> a(n)
+#define fin(i,x,n,a) for(i=x;i<n;i++) cin>>a[i]
+
+class Solution {
+public:
+    void solve()
+    {
+        
+    }
+};
+
+int main() {
+    Solution s;
+    int tc;
+    cin >> tc;
+    while (tc-- > 0)
+    {
+        s.solve();
+    }
+    return 0;
+}
