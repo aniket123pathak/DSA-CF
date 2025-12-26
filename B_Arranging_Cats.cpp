587b80e689684ee12b8dd3408c809876955f989d
Aniket Pathak <aniketpathak581@gmail.com>
2025-12-26 09:15:50 +0530

    new que

---
+++ B_Arranging_Cats.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define ll long long
+#define v(a,n) vector<long long> a(n)
+#define fin(i,x,n,a) for(i=x;i<n;i++) cin>>a[i]
+
+/*
+
+
+*/
+
+class Solution {
+public:
+    void solve()
+    {
+        ll n,i;
+        cin>>n;
+        string e,f;
+        cin>>e;
+        cin>>f;
+        ll count=0;
+        for(i=0;i<n;i++){
+            if(e[i]!=f[i]){
+                count++;
+            }
+        }
+        cout<<count<<endl;
+    }
+};
+
+int main() {
+    Solution s;
+    int tc;
+    cin >> tc;
+    while (tc-- > 0)
+    {
+        s.solve();
+    }
+    return 0;
+}
