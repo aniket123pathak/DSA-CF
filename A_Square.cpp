6ee4a8a8b78e1f08aeebe92f2670952269f402fc
Aniket Pathak <aniketpathak581@gmail.com>
2025-12-26 09:15:44 +0530

    new que

---
+++ A_Square.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define ll long long
+#define v(a,n) vector<long long> a(n)
+#define fin(i,x,n,a) for(i=x;i<n;i++) cin>>a[i]
+
+class Solution {
+public:
+    void solve()
+    {
+        ll x1,y1,x2,y2,x3,y3,x4,y4;
+        cin>>x1>>y1>>x2>>y2>>x3>>y3>>x4>>y4;
+
+        if(x2-x1!=0){
+        cout<<(x2-x1)*(x2-x1)<<endl;
+        }
+        else{
+            cout<<(y2-y1)*(y2-y1)<<endl;
+        }
+    }
+};
+
+int main() {
+    Solution s;
+    int tc;
+    cin >> tc;
+    while (tc-- > 0)
+    {
+        s.solve();
+    }
+    return 0;
+}
