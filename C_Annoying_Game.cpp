e210a64ea309d3c33f12cbf4b6f616f0ab872566
Aniket Pathak <aniketpathak581@gmail.com>
2026-01-19 11:42:28 +0530

    Bug Fix

--- C_Annoying_Game.cpp
+++ C_Annoying_Game.cpp
@@ -22,9 +22,12 @@ public:
     {
         long long n, k, i;
         cin >> n >> k;
-        vector<long long> a(n);
+        vector<long long> a(n),sumArr(n);
         for (i = 0; i < n; i++){
             cin >> a[i];
+            sumArr[0]=a[0];
+            if(i>0)
+                sumArr[i]+=sumArr[i-1]+a[i];
         }
         vector<long long> b(n);
         for (i = 0; i < n; i++)
@@ -46,20 +49,20 @@ public:
         for (long long k = 1; k < a.size(); k++)
         {
             sum = max(a[k], sum+a[k]);
-            best = max(best, sum);
-            dp1[k]=best;
+            dp1[k]=sum;
         }
 
         dp2[0]=a[0]+b[0];
 
         for(i=1;i<a.size();i++){
-            long long max2 = max(dp2[i-1],dp1[i-1]+a[i]+b[i]);
-            long long max1 = max(dp2[i-1],max2);
-            dp2[i]=max(max1,max2);
-
+            long long max1 = a[i]+b[i];
+            long long max2 = max(dp1[i-1]+max1,max1);
+            dp2[i]=max(dp2[i-1]+a[i],max2);
         }
 
         cout << *max_element(dp2.begin(),dp2.end()) << endl;
+
+
     }
 };
 
