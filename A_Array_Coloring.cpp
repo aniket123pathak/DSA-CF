dbd42f67bd8a0a13a396b9a67362f76939ebacf9
Aniket Pathak <aniketpathak581@gmail.com>
2026-01-19 10:05:26 +0530

    new que

---
+++ A_Array_Coloring.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define ll long long
+#define v(a,n) vector<long long> a(n)
+#define fin(i,x,n,a) for(i=x;i<n;i++) cin>>a[i]
+
+class Solution {
+public:
+    void solve()
+    {
+        ll n,i;
+        cin>>n;
+        vector<ll>a(n);
+        for(i=0;i<n;i++){
+           cin>>a[i];
+        }
+
+        for(i=0;i<n-1;i++){
+            if(abs(a[i+1]-a[i])%2==0){
+                cout<<"NO\n";
+                return;
+            }
+        }
+        cout<<"YES\n";
+    }
+};
+
+int main() {
+    Solution s;
+    int tc;
+    cin >> tc;
+    while (tc-- > 0)
+    {
+        s.solve();
+    }
+    return 0;
+}
