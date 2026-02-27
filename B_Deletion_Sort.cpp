ed5e29e7ebfda42023e846a8f8e6c513830d6dae
Aniket Pathak <aniketpathak581@gmail.com>
2026-02-27 22:04:02 +0530

    new que

---
+++ B_Deletion_Sort.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define v(a,n) vector<long long> a(n)
+#define fin(i,x,n,a) for(i=x;i<n;i++) cin>>a[i]
+
+class Solution {
+public:
+    void solve()
+    {
+        ll n,i;
+        cin>>n;
+        vector<ll>a(n);
+        for(i=0;i<n;i++){
+           cin>>a[i];
+        }
+        bool check = true;
+
+        for(i=0;i<n-1;i++){
+            if(a[i+1]<a[i]){
+                check=false;
+                break;
+            }
+        }
+        if(check){
+            cout<<n<<endl;
+        }
+        else{
+            cout<<1<<endl;
+        }
+        
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    Solution s;
+    int tc;
+    cin >> tc;
+    while (tc-- > 0)
+    {
+        s.solve();
+    }
+    return 0;
+}
