abb9ce1542fe0b2bdce6cfea44f03c746a9a1b7b
Aniket Pathak <aniketpathak581@gmail.com>
2026-02-27 22:03:56 +0530

    new que

---
+++ A_Trippi_Troppi.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define v(a,n) vector<long long> a(n)
+#define fin(i,x,n,a) for(i=x;i<n;i++) cin>>a[i]
+
+class Solution {
+public:
+    void solve()
+    {
+        string s1="",s2="",s3="";
+        cin>>s1>>s2>>s3;
+        cout<<s1[0]<<s2[0]<<s3[0]<<endl;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    Solution s;
+    int tc;
+    cin >> tc;
+    while (tc-- > 0)
+    {
+        s.solve();
+    }
+    return 0;
+}
