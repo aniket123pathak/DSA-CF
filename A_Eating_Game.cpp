05f9b53bb55348ebcaa8ab073ffc743b05be661b
Aniket Pathak <aniketpathak581@gmail.com>
2026-02-27 22:03:51 +0530

    new que

---
+++ A_Eating_Game.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define v(a,n) vector<long long> a(n)
+#define fin(i,x,n,a) for(i=x;i<n;i++) cin>>a[i]
+
+class Solution {
+public:
+    void solve()
+    {
+        ll n,i;
+        cin>>n;
+        vector<ll>a(n);
+        for(i=0;i<n;i++){
+           cin>>a[i];
+        }
+        ll mx = *max_element(a.begin(),a.end());
+        ll ans=0;
+        for(i=0;i<n;i++){
+            if(a[i]==mx){
+                ans++;
+            }
+        }
+        cout<<ans<<endl;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    Solution s;
+    int tc;
+    cin >> tc;
+    while (tc-- > 0)
+    {
+        s.solve();
+    }
+    return 0;
+}
