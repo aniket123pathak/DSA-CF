ca8d0e438877a540a4a433ce39fd595f45d69bb0
Aniket Pathak <aniketpathak581@gmail.com>
2026-02-28 23:34:08 +0530

    new que

---
+++ A_Maximize_the_Last_Element.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define v(a,n) vector<long long> a(n)
+#define fin(i,x,n,a) for(i=x;i<n;i++) cin>>a[i]
+
+class Solution {
+public:
+    void solve()
+    {
+        ll n,i;
+        cin>>n;
+        vector<ll>a(n);
+        ll mx = -1;
+        for(i=0;i<n;i++){
+           cin>>a[i];
+            if(i%2==0){
+                mx=max(mx,a[i]);
+            }
+        }
+        cout<<mx<<endl;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    Solution s;
+    int tc;
+    cin >> tc;
+    while (tc-- > 0)
+    {
+        s.solve();
+    }
+    return 0;
+}
