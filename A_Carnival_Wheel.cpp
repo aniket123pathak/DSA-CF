918e1dc2111b4c850fe52804b9fb9d49c5e64de5
Aniket Pathak <aniketpathak581@gmail.com>
2026-03-01 13:23:20 +0530

    new que

--- A_Carnival_Wheel.cpp
+++ A_Carnival_Wheel.cpp
@@ -9,10 +9,6 @@ class Solution {
         {
             long long n,a,b;
             cin>>n>>a>>b;
-            if(a==0){
-
-            }
-
             long long maxP = INT64_MIN;
             for(int i=1;i<100000;i++){
                 maxP=max(maxP,(a+i*b)%n);
