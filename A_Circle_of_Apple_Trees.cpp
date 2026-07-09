26fdf8bee2d8242900d070a592fdc006730b4618
aniket123pathak <aniketpathak581@gmail.com>
2026-07-09 21:48:12 +0530

    new que

---
+++ A_Circle_of_Apple_Trees.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define v(a,n) vector<long long> a(n)
+#define fin(i,x,n,a) for(i=x;i<n;i++) cin>>a[i]
+
+class Solution {
+public:
+    void solve()
+    {
+        ll n,i;
+        cin>>n;
+        vector<ll>a(n);
+        for(i=0;i<n;i++){
+           cin>>a[i];
+        }
+        sort(a.begin(),a.end());
+        ll apple = 1;
+        ll prev = a[0];
+        for(i=1;i<n;i++){
+            if(a[i]>prev){
+                apple++;
+                prev=a[i];
+            }
+        }
+        cout<<apple<<endl;
+        return;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    Solution s;
+    int tc;
+    cin >> tc;
+    while (tc-- > 0)
+    {
+        s.solve();
+    }
+    return 0;
+}
