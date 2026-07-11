ff2901d6600182d06c28dcfcf1dc30512f8ac108
aniket123pathak <aniketpathak581@gmail.com>
2026-07-11 22:13:20 +0530

    new que

---
+++ A_Submission_is_All_You_Need.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define v(a,n) vector<long long> a(n)
+#define fin(i,x,n,a) for(i=x;i<n;i++) cin>>a[i]
+
+class Solution {
+public:
+    void solve()
+    {
+        ll n,i;
+        cin>>n;
+        vector<ll>a(n);
+        ll sum = 0;
+
+        for(i=0;i<n;i++){
+           cin>>a[i];
+           sum+=a[i];
+           if(a[i]==0){
+            sum++;
+           }
+        }
+        cout<<sum<<endl;
+        
+        return;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    Solution s;
+    int tc;
+    cin >> tc;
+    while (tc-- > 0)
+    {
+        s.solve();
+    }
+    return 0;
+}
