04b3021ffa3f9c3bafdef014bc9ef6fddd8fb9f7
aniket123pathak <aniketpathak581@gmail.com>
2026-07-12 18:12:28 +0530

    new que

---
+++ A_Omkar_and_Password.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define v(a,n) vector<long long> a(n)
+#define fin(i,x,n,a) for(i=x;i<n;i++) cin>>a[i]
+
+class Solution {
+public:
+    void solve()
+    {
+        ll n,i;
+        cin>>n;
+        vector<ll>a(n);
+        for(i=0;i<n;i++){
+           cin>>a[i];
+        }
+        bool check = true;
+        ll prev = a[0];
+        for(i=1;i<n;i++){
+            if(a[i]!=prev){
+                check = false;
+                break;
+            }
+        }
+
+        if(check){
+            cout<<n<<endl;
+        }
+        else{
+            cout<<1<<endl;
+        }
+        
+        return;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    Solution s;
+    int tc;
+    cin >> tc;
+    while (tc-- > 0)
+    {
+        s.solve();
+    }
+    return 0;
+}
