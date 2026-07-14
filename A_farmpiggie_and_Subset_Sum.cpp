0ddb697726f6de0d252f81e6f343e32c1cb77bbc
aniket123pathak <aniketpathak581@gmail.com>
2026-07-14 11:41:39 +0530

    new que

---
+++ A_farmpiggie_and_Subset_Sum.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define v(a,n) vector<long long> a(n)
+#define fin(i,x,n,a) for(i=x;i<n;i++) cin>>a[i]
+
+class Solution {
+public:
+    void solve()
+    {
+        ll n,i;
+        cin>>n;
+        for(i=2;i<=n;i++){
+            cout<<i<<" ";
+        }
+        cout<<1<<endl;
+        
+        return;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    Solution s;
+    int tc;
+    cin >> tc;
+    while (tc-- > 0)
+    {
+        s.solve();
+    }
+    return 0;
+}
