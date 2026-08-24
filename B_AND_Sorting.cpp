87bd33e38759fd11ae55953fe7111d94e6a9d21a
aniket123pathak <aniketpathak581@gmail.com>
2026-08-24 18:30:42 +0530

    new que

---
+++ B_AND_Sorting.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define v(a,n) vector<long long> a(n)
+#define fin(i,x,n,a) for(i=x;i<n;i++) cin>>a[i]
+
+class Solution {
+public:
+    void solve()
+    {
+        ll n,i;
+        cin>>n;
+        vector<ll>a(n);
+        for(i=0;i<n;i++){
+           cin>>a[i];
+        }
+        ll ans = (1<<30)-1;
+        for(i=0;i<n;i++){
+            if(a[i]!=i){
+                ans = ans & a[i];
+            }
+        }
+        cout<<ans<<endl;
+        return;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    Solution s;
+    int tc;
+    cin >> tc;
+    while (tc-- > 0)
+    {
+        s.solve();
+    }
+    return 0;
+}
